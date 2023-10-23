ded9fda66f84e49aabbaf8c63e76d928edc2c049
Mika Sato <mika99@live.com.au>
2023-10-23 15:35:47 +1000

    solve product-of-array-except-self problem

--- neetcode/arrays-hashing/product-of-array-except-self.ts
+++ neetcode/arrays-hashing/product-of-array-except-self.ts
@@ -1,21 +1,26 @@
 function productExceptSelf(nums: number[]): number[] {
-    // Create a new object to store each productExceptSelf
-    const products: {
-        [key: number]: number
-    } = {};
+    // Create a new array to store each productExceptSelf
+    const productsArr: number[] = [];
 
-    // Loop through each element in the nums array
+    // Set product to 1 initially
+    let product: number = 1;
+
+    // Loop through each element in nums array to get LHS product
     for (let i = 0; i < nums.length; i++) {
-        const numsExceptSelf: number[] = nums.slice();  // copy nums array (so it isn't mutated)
-        numsExceptSelf.splice(i, 1);    // remove self element
-
-        products[i] = numsExceptSelf.reduce((a, b) => a * b, 1); // get the product of all elements
+        productsArr[i] = product;
+        product *= nums[i];
     };
+    
+    // Reset product to 1
+    product = 1;
 
-    // Return the answer array with each productExceptSelf
-    const answer: number[] = Object.values(products);
+    // Loop through each element again to get RHS product
+    for (let i = nums.length - 1; i >= 0; i--) {
+        productsArr[i] *= product;
+        product *= nums[i];
+    }
 
-    return answer;
+    return productsArr;
 };
 
 console.log('result', productExceptSelf([-1,1,0,-3,3]));
