eeba6b889f92967260837e1f34ab849548ab5da4
Mika Sato <mika99@live.com.au>
2023-11-03 17:57:36 +1000

    solve longest-consec-seq problem

--- neetcode/arrays-hashing/longest-conseq-seq.ts
+++ neetcode/arrays-hashing/longest-conseq-seq.ts
@@ -1,41 +1,30 @@
 function longestConsecutive(nums: number[]): number {
-    let result: number;
+    // Create a variable to store the longest consec seq
+    let longestSeq: number = 0;
+
     // Check if nums is empty
     if (nums.length == 0) {
-        result = 0;
-        return result;
-    } else {
-        result = 1;
+        return longestSeq;
     }
-
-    // Create an array to store each sequence length
-    const seqLengths: number[] = [];
 
     // Create a set (to hold only unique values)
     const numsSet: Set<number> = new Set(nums);
 
-    // Find min num in numsSet to use as the starting point
-    const minNum: number = Math.min(...numsSet);
-    
-    // Check if each consecutive num exists in numsSet
-    let currentNum: number = minNum;
-    let currentSeqLength: number = 1;
-    for (let i = 1; i < numsSet.size; i++) {
-        if (!numsSet.has(currentNum)) {
-            continue;
-        } else if (numsSet.has(currentNum + 1)) {
-            currentSeqLength += 1;
+    // Loop through each num to get the consec seq length starting from each
+    for (const num of numsSet) {
+        if (numsSet.has(num - 1)) {
+            continue;   // will count the length from the prev number instead
         } else {
-            seqLengths.push(currentSeqLength);
-            currentSeqLength = 1;
+            let length: number = 0;
+            while (numsSet.has(num + length)) {
+                length += 1;    // increment length each time while next num in seq exists
+            }
+            longestSeq = Math.max(longestSeq, length);
+            console.log("num: " + num + ", longestSeq: " + longestSeq);
         }
-        currentNum += 1;
     }
-    seqLengths.push(currentSeqLength);
 
-    // Set result as the max value in seqLengths
-    result = Math.max(...seqLengths);
-    return result;
+    return longestSeq;
 };
 
-console.log('result', longestConsecutive([9,1,4,7,3,-1,0,5,8,-1,6]));
+console.log('result', longestConsecutive([100,4,200,1,3,2]));
